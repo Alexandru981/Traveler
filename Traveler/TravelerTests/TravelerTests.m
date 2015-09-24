74e62dda02f0e133d26df411018c264860457c32
Alexandru Miculescu <alex@flypay.co.uk>
2015-09-24 13:36:02 +0100

    First Commit! YEY!

---
+++ Traveler/TravelerTests/TravelerTests.m
@@ -0,0 +1,39 @@
+//
+//  TravelerTests.m
+//  TravelerTests
+//
+//  Created by Alexandru Miculescu on 24/09/2015.
+//  Copyright © 2015 Alexandru Miculescu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TravelerTests : XCTestCase
+
+@end
+
+@implementation TravelerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
