f17afa715e5398e0f5447472006a9fb6babfc40b
Alexandru Miculescu <alex@flypay.co.uk>
2015-10-06 23:40:17 +0100

    Add TRLocationManager to the app

---
+++ Traveler/Traveler/Managers/TRLocationManager.h
@@ -0,0 +1,15 @@
+//
+//  TRLocationManager.h
+//  Traveler
+//
+//  Created by Alexandru Miculescu on 06/10/2015.
+//  Copyright © 2015 Alexandru Miculescu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TRLocationManager : NSObject
+
++ (instancetype)sharedInstance;
+
+@end
